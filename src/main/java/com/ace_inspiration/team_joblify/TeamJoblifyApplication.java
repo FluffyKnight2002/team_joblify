89dc7c7fde8a8b128750f71153a25dff2068d310
XtraYuki <aungkhentzaw@gmail.com>
2023-07-04 10:17:16 +0630

    inital project

---
+++ src/main/java/com/ace_inspiration/team_joblify/TeamJoblifyApplication.java
@@ -0,0 +1,13 @@
+package com.ace_inspiration.team_joblify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamJoblifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamJoblifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ace_inspiration/team_joblify/TeamJoblifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ace_inspiration.team_joblify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamJoblifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
