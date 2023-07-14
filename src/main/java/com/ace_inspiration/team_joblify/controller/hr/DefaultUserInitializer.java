c786d5b97424d43aa9923d6678cfb7e6294feaed
XtraYuki <aungkhentzaw@gmail.com>
2023-07-14 10:31:54 +0630

    Initiallizer

--- src/main/java/com/ace_inspiration/team_joblify/controller/hr/DefaultUserInitializer.java
+++ src/main/java/com/ace_inspiration/team_joblify/controller/hr/DefaultUserInitializer.java
@@ -5,15 +5,20 @@ import com.ace_inspiration.team_joblify.entity.*;
 import com.ace_inspiration.team_joblify.repository.ActionRepository;
 import com.ace_inspiration.team_joblify.repository.DepartmentRepository;
 import com.ace_inspiration.team_joblify.repository.UserRepository;
+import jakarta.servlet.ServletContext;
 import lombok.AllArgsConstructor;
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-
+import org.springframework.util.StreamUtils;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.time.LocalDateTime;
 
 @Component
@@ -24,14 +29,27 @@ public class DefaultUserInitializer implements ApplicationRunner {
     private final UserRepository userRepository;
     private final ActionRepository actionRepository;
     private final PasswordEncoder passwordEncoder;
+    private final ServletContext servletContext;
+
 
 
     @Value("${app.default-user-password}")
     private static String password;
 
+
+
     @Override
     public void run(ApplicationArguments args) throws IOException {
+
+        String fullPath = servletContext.getRealPath("/static/assets/images/faces/5.jpg");
+        File file = new File(fullPath);
+        byte[] fileBytes;
+        try (InputStream inputStream = new FileInputStream(file)) {
+            fileBytes = StreamUtils.copyToByteArray(inputStream);
+        }
+
         LocalDateTime currentDate= LocalDateTime.now();
+
         Department department=departmentRepository.findById(1L).orElseThrow(()-> new UsernameNotFoundException("Department Not Found"));
         if (userRepository.count() == 0) {
             User defaultUser = User.builder()
@@ -40,7 +58,7 @@ public class DefaultUserInitializer implements ApplicationRunner {
                     .phone("09777159555")
                     .email("ace@gmail.com")
                     .address("Ace Data System")
-                    .photo(photo)
+                    .photo(Base64.encodeBase64String(fileBytes))
                     .gender(Gender.FEMALE)
                     .password(passwordEncoder.encode(password))
                     .role(Role.DEFAULT_HR)
