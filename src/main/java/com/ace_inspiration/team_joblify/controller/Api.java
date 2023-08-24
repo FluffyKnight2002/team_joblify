9338b94d5bc9094cf581dcbf6fd7021ad423c65e
XtraYuki <aungkhentzaw@gmail.com>
2023-08-24 16:50:08 +0630

    aaaa

--- src/main/java/com/ace_inspiration/team_joblify/controller/Api.java
+++ src/main/java/com/ace_inspiration/team_joblify/controller/Api.java
@@ -54,44 +54,58 @@ public class Api {
     public ResponseEntity<User> userRegister(UserDto userDto, Authentication authentication) throws IOException {
         MyUserDetails myUserDetails = (MyUserDetails) authentication.getPrincipal();
         User user = userService.userCreate(userDto, myUserDetails.getUserId());
-        if(user != null){
+        if (user != null) {
             String message = myUserDetails.getName() + " create a new User named" + user.getName();
             String link = "/user-profile-edit?id=" + user.getId();
-            notificationCreator.createNotification(myUserDetails ,message,link);
+            notificationCreator.createNotification(myUserDetails, message, link);
         }
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     @PostMapping(value = "/user-profile-edit", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<User> userProfileEdit(UserDto userDto, @RequestParam("currentEmail") String currentEmail, Authentication authentication) throws IOException {
-        System.out.println(userDto);
-        System.out.println(currentEmail);
+    public ResponseEntity<User> userProfileEdit(UserDto userDto, @RequestParam("currentEmail") String currentEmail,
+            Authentication authentication) throws IOException {
         MyUserDetails myUserDetails = (MyUserDetails) authentication.getPrincipal();
         User user;
-        if (myUserDetails.getAuthorities().stream().anyMatch(authority -> authority.getAuthority().equals(Role.DEFAULT_HR.name()))) {
-            user = userService.adminProfileEdit(userDto, currentEmail);
-            
-        } else {
-            user = userService.userProfileEdit(userDto, currentEmail);
-            
-        }
 
-        if (myUserDetails.getEmail().equals(currentEmail)) {
-            MyUserDetails myNewUserDetails = new MyUserDetails(user);
-            Authentication newAuthentication = new UsernamePasswordAuthenticationToken(myNewUserDetails, myNewUserDetails.getPassword(), myNewUserDetails.getAuthorities());
-            SecurityContextHolder.getContext().setAuthentication(newAuthentication);
+        // Check if the authenticated user has the role "Default HR"
+        boolean isDefaultHr = myUserDetails.getAuthorities().stream()
+                .anyMatch(authority -> authority.getAuthority().equals(Role.DEFAULT_HR.name()));
+
+        // Check if the user is trying to edit their own profile
+        boolean isEditingOwnProfile = currentEmail.equals(myUserDetails.getEmail());
+
+        if (isDefaultHr || isEditingOwnProfile) {
+            // If Default HR or editing own profile, allow profile edit
+            if (isDefaultHr) {
+                user = userService.adminProfileEdit(userDto, currentEmail);
+            } else {
+                user = userService.userProfileEdit(userDto, currentEmail);
+            }
+
+            if (myUserDetails.getEmail().equals(currentEmail)) {
+                MyUserDetails myNewUserDetails = new MyUserDetails(user);
+                Authentication newAuthentication = new UsernamePasswordAuthenticationToken(myNewUserDetails,
+                        myNewUserDetails.getPassword(), myNewUserDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(newAuthentication);
+            }
+
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } else {
+            // If not Default HR and not editing own profile, return a 403 response
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
         }
-        
-        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     @PostMapping("/change-password")
-    public boolean changePassword(@RequestParam("newPassword") String newPassword, @RequestParam("email") String email) {
+    public boolean changePassword(@RequestParam("newPassword") String newPassword,
+            @RequestParam("email") String email) {
         return userService.passwordChange(newPassword, email);
     }
 
     @PostMapping("/old-password-check")
-    public boolean oldPasswordCheck(@RequestParam("oldPassword") String oldPassword, @RequestParam("email") String email) {
+    public boolean oldPasswordCheck(@RequestParam("oldPassword") String oldPassword,
+            @RequestParam("email") String email) {
         return userService.checkOldPassword(oldPassword, email);
     }
 
@@ -103,13 +117,14 @@ public class Api {
 
     @PostMapping("/send-invite-email")
     public String sendInviteEmail(@RequestBody EmailTemplateDto emailTemplateDto) {
-    	System.err.println(">>>>>>>>>"+emailTemplateDto.getCanId()+">>>"+InterviewType.valueOf(emailTemplateDto.getType()));
+        System.err.println(
+                ">>>>>>>>>" + emailTemplateDto.getCanId() + ">>>" + InterviewType.valueOf(emailTemplateDto.getType()));
         emailService.sendJobOfferEmail(emailTemplateDto.getTo(), emailTemplateDto.getContent());
 
         Candidate candidate = new Candidate();
         candidate.setId(emailTemplateDto.getCanId());
 
-        Interview interview=new Interview();
+        Interview interview = new Interview();
         interview.setInterviewDate(emailTemplateDto.getDate());
         interview.setInterviewTime(emailTemplateDto.getTime());
         interview.setType(InterviewType.valueOf(emailTemplateDto.getType()));
@@ -138,72 +153,74 @@ public class Api {
     }
 
     @PostMapping("/suspend")
-    public boolean suspend(@RequestParam("id")long id){
+    public boolean suspend(@RequestParam("id") long id) {
         return userService.suspend(id);
     }
 
     @PostMapping("/activate")
-    public boolean activate(@RequestParam("id")long id){
+    public boolean activate(@RequestParam("id") long id) {
         return userService.activate(id);
     }
 
     @PostMapping("/get-user-profile")
-    public User userProfileData(@RequestParam ("email") String email){
+    public User userProfileData(@RequestParam("email") String email) {
         return userService.findByEmail(email);
     }
 
     @PostMapping("/find-phonenumber-by-email")
-    public String findPhoneNumberByEmail(@RequestParam ("email") String email){
-        
+    public String findPhoneNumberByEmail(@RequestParam("email") String email) {
+
         User user = userService.findByEmail(email);
         System.out.println(user.getPhone());
         return user.getPhone();
     }
 
     @GetMapping("/phone-duplicate")
-    public boolean checkPhoneDuplicate(@RequestParam("phone")String phone){
+    public boolean checkPhoneDuplicate(@RequestParam("phone") String phone) {
         return userService.checkPhoneDuplicate(phone);
     }
 
-
     @GetMapping("/username-duplicate")
-    public boolean checkUsernameDuplicate(@RequestParam("username")String username){
+    public boolean checkUsernameDuplicate(@RequestParam("username") String username) {
         return userService.checkUsernameDuplicate(username);
     }
 
     @GetMapping("/email-duplicate")
     public boolean emailDuplicateSearch(@RequestParam("email") String email) {
         return userService.emailDuplication(email);
-}
+    }
 
     @GetMapping("/phone-duplicate-except-himself")
-    public boolean checkPhoneDuplicateExceptHimself(@RequestParam("newPhone")String newPhone, @RequestParam("userId")long userId){
+    public boolean checkPhoneDuplicateExceptHimself(@RequestParam("newPhone") String newPhone,
+            @RequestParam("userId") long userId) {
         System.out.println(userId);
         return userService.checkPhoneDuplicateExceptHimself(newPhone, userId);
     }
 
-
     @GetMapping("/username-duplicate-except-himself")
-    public boolean checkUsernameDuplicateExceptHimself(@RequestParam("newUsername")String newUsername, @RequestParam("userId")long userId){
+    public boolean checkUsernameDuplicateExceptHimself(@RequestParam("newUsername") String newUsername,
+            @RequestParam("userId") long userId) {
         return userService.checkUsernameDuplicateExceptHimself(newUsername, userId);
     }
 
     @GetMapping("/email-duplicate-except-himself")
-    public boolean emailDuplicateSearchExceptHimself(@RequestParam("newEmail") String newEmail, @RequestParam("userId")long userId) {
+    public boolean emailDuplicateSearchExceptHimself(@RequestParam("newEmail") String newEmail,
+            @RequestParam("userId") long userId) {
         return userService.emailDuplicationExceptHimself(newEmail, userId);
-}
+    }
 
     @PostMapping("/authenticated-user-data")
     public MyUserDetails loginUserData(Authentication authentication) {
-        MyUserDetails myUserDetails = (MyUserDetails)authentication.getPrincipal();
+        MyUserDetails myUserDetails = (MyUserDetails) authentication.getPrincipal();
         return myUserDetails;
-}
+    }
 
-//    @GetMapping("/filtered-vacancies")
-//    public List <Object[]> getFilteredVacancies() {
-//        List<Object[]> result = vacancyInfoRepository.vacancyFilter("recent", true, null, "BOTH", null, false, "anytime", 1, 1);
-//        System.out.println(result);
-//
-//        return result;
-//    }
+    // @GetMapping("/filtered-vacancies")
+    // public List <Object[]> getFilteredVacancies() {
+    // List<Object[]> result = vacancyInfoRepository.vacancyFilter("recent", true,
+    // null, "BOTH", null, false, "anytime", 1, 1);
+    // System.out.println(result);
+    //
+    // return result;
+    // }
 }
