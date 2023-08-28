e3d0d50051d22184d06ebde5c4d158c0826f0373
XtraYuki <aungkhentzaw@gmail.com>
2023-08-28 15:20:02 +0630

    side bar

--- src/main/java/com/ace_inspiration/team_joblify/controller/hr/InterviewController.java
+++ src/main/java/com/ace_inspiration/team_joblify/controller/hr/InterviewController.java
@@ -2,6 +2,7 @@ package com.ace_inspiration.team_joblify.controller.hr;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -12,14 +13,14 @@ public class InterviewController {
 	
 	
 	@GetMapping("/interview-process")
-	public String interviewProcessSummary() {
-		
+	public String interviewProcessSummary(Model model) {
+		model.addAttribute("currentPage", "/interview-process");
 		return "interviewproces-summary";
 	}
 	
 	@GetMapping("/candidate-view-summary")
-	public String all_candidtate_view_summary() {
-		
+	public String all_candidtate_view_summary(Model model) {
+		model.addAttribute("currentPage", "/candidate-view-summary");
 		return "all-candidate-view-summary";
 	}
 	
